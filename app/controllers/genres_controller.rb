686590c88e857bf772c9ef4dc20b03475d770535
HMoreira2020 <moreiraheather@gmail.com>
2020-08-15 12:55:54 -0700

    Build new, create, show and index actions for GenresController

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,15 +1,24 @@
 class GenresController < ApplicationController
     def index
+        @genres = Genre.all 
     end 
 
     def new
+        @genre = Genre.new 
     end
 
     def create
+        @genre = Genre.new(genre_params)
+        if @genre.save 
+            redirect_to genre_path(@genre)
+        else 
+            render :new, alert: "Genre Name Required"
+        end
     end
     
 
     def show
+        @genre = Genre.find_by(id: params[:id])
     end
 
 

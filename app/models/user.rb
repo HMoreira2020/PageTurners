e268ffbf0bb7066e623fb1aa7c08c4dd1b3bf0dd
HMoreira2020 <moreiraheather@gmail.com>
2020-08-15 13:14:33 -0700

    Add validations to user and add vaild_email2 gem to GGemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
     has_many :lists #that they created 
     has_many :reviewed_books, through: :reviews, source: :book #that they have reviewed
     has_many :books #that they have created 
-    has_secure_password 
+    
     #add validations name, email username, password 
+    validates :name, uniqueness: true, presence: true 
+    validates :username, uniqueness: true, presence: true
+    validates :email, uniqueness: true, presence: true, 'valid_email_2/email': true
 
+    has_secure_password 
 end

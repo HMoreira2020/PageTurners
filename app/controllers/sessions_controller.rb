876ed46ccd6502b38116e2d36e35d3d774cd7983
HMoreira2020 <moreiraheather@gmail.com>
2020-08-15 12:28:31 -0700

    Redirect user from homepage to books index if logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,11 @@
 class SessionsController < ApplicationController
 
     def welcome
+        if logged_in? 
+            redirect_to books_path 
+        else 
+            render :welcome
+        end
     end 
 
     def new

461d1da0137a3f883bd7498b4c422bf2c04ca63a
HMoreira2020 <moreiraheather@gmail.com>
2020-08-15 12:53:31 -0700

    Build CRUD actions in ListsController

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,21 @@
 class ListsController < ApplicationController
+    before_action :set_list, only: [:show, :edit, :update, :destroy]
+
     def index
+        @lists = List.all 
     end 
 
     def new
+        @list = List.new 
     end
 
     def create
+        @list = List.new(list_params)
+        if @list.save 
+            redirect_to user_path(current_user)
+        else 
+            render :new, alert: "List name required"
+        end 
     end
     
 
@@ -16,9 +26,28 @@ class ListsController < ApplicationController
     end
 
     def update
+        @list.update(list_params)
+        if @list.save 
+            redirect_to list_path(@list)
+        else 
+            render :edit, alert: "Title required"
+        end
     end
 
     def destroy 
+        @list.destroy 
+        redirect_to user_path(current_user)
+    end 
+
+    private 
+
+    def list_params
+        params.require(list).permit(:title)
+    end 
+
+
+    def set_list 
+        @list = List.find_by(id: params[:id])
     end 
 
 end

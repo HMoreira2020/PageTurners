d215af7f0f875cf91e0ffc4e57f70bc73d0bdbae
HMoreira2020 <moreiraheather@gmail.com>
2020-08-15 12:41:40 -0700

    Add Login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,22 @@ class SessionsController < ApplicationController
     end 
 
     def new
+        
     end
 
     def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else 
+            flash[:alert] = "Login is incorrect"
+            redirect_to login_path
+        end 
     end 
 
     def destroy
+        session.delete(:user_id)
+        redirect_to root_path
     end 
 end

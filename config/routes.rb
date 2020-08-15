03c5a76828fd1e4736d090e98b5beaf887743c52
HMoreira2020 <moreiraheather@gmail.com>
2020-08-15 13:54:10 -0700

    Stub out usser new list form  and user lists index views

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   resources :genres
   resources :reviews
   resources :books
-  resources :lists
-  resources :users
+ 
+  resources :users do 
+    resources :lists
+  end 
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

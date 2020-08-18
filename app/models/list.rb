d7bacf07e96f2f3fa4cc217c9b8666354ff12a50
HMoreira2020 <moreiraheather@gmail.com>
2020-08-17 21:52:03 -0700

    Create List create action to include nested attributes for book and collection select

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,8 +5,19 @@ class List < ApplicationRecord
   #validations for title
   validates :title, presence: true, uniqueness: { scope: :user_id,
   message: "You already have a list with this title." }
-  accepts_nested_attributes_for_books
+  
+  def book_attributes=(book_hash)
+    book_hash.values.each do |book_attributes|
+      self.books.build(book_attributes)
+    end 
+  end 
 
+  def book_ids=(ids)
+    ids.each do |id|
+      book = Book.find(id)
+      self.books << book
+    end
+  end
   
 
   
